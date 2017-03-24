12cd210b54784bb7e778a870845b1abfdf5d438d
Patrick Loughrey <PatrickLoughrey@nwk--198-161.nwk.ruw.rutgers.edu>
2017-03-24 16:58:33 -0400

    Inserted tables into database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+/* Insert values into the burgers database */
+INSERT INTO burgers (burger_name, devoured) 
+  VALUES ('Lamb Burger', TRUE), ('Turkey Burger', TRUE), ('Veggie Burger', FALSE), ('Crab Cake Burger', TRUE), ('Angus Beef Burger', TRUE);
